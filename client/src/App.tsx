decd050e564833a8e91149ac942269939a8b70f0
NAM NGUYEN <Nam.Nguyen@ibm.com>
2022-02-11 22:54:46 -0500

    add linting and formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+export type AppProps = {};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC<AppProps> = (props) => {
+  const {} = props;
+
+  return <div>App work!</div>;
+};
 
 export default App;
